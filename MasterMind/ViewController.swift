e74532cb8eca1e66838dc15a07112d2623b0bfba
Bernardo Trevino <bernardotc94@gmail.com>
2016-08-24 22:10:35 -0500

    Randomized the initial guess, and enabled option to reset game

--- MasterMind/ViewController.swift
+++ MasterMind/ViewController.swift
@@ -17,15 +17,51 @@ class ViewController: UIViewController {
     let colorPurple = UIColor.purpleColor()
     let colorCyan = UIColor.cyanColor()
     let colorWhite = UIColor.whiteColor()
-
+    
+    @IBOutlet weak var viewGuessFirst: UIView!
+    @IBOutlet weak var viewGuessSecond: UIView!
+    @IBOutlet weak var viewGuessThird: UIView!
+    @IBOutlet weak var viewGuessFourth: UIView!
+    @IBOutlet weak var btnGuessFirst: UIButton!
+    @IBOutlet weak var btnGuessSecond: UIButton!
+    @IBOutlet weak var btnGuessThird: UIButton!
+    @IBOutlet weak var btnGuessFourth: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resetCompleteGuess()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func resetCompleteGuess() {
+        viewGuessFirst.backgroundColor = randomizeGuess()
+        viewGuessSecond.backgroundColor = randomizeGuess()
+        viewGuessThird.backgroundColor = randomizeGuess()
+        viewGuessFourth.backgroundColor = randomizeGuess()
+    }
+    
+    func randomizeGuess() -> UIColor {
+        let randomNumber = Int(arc4random_uniform(6) + 1)
+        switch randomNumber {
+            case 1: return colorGreen
+            case 2: return colorOrange
+            case 3: return colorBlue
+            case 4: return colorPurple
+            case 5: return colorRed
+            default: return colorCyan
+        }
+    }
+    
+    @IBAction func resetGame(sender: UIButton) {
+        resetCompleteGuess()
+        btnGuessFirst.backgroundColor = colorGreen
+        btnGuessSecond.backgroundColor = colorGreen
+        btnGuessThird.backgroundColor = colorGreen
+        btnGuessFourth.backgroundColor = colorGreen
     }
 
     @IBAction func changeColor(sender: UIButton) {
